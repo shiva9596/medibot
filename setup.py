99b1b854cba25d7cb69d04a03a6c1e778f60e616
shiva9596 <gouds5788@gmail.com>
2025-04-12 16:59:48 -0700

    Folder structure created

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot using Generative AI Project',
+    version= '0.0.0',
+    author= 'Shiva',
+    author_email= 'skp5.aiml@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
