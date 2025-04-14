87e39d27d4126dcc06399e363142804d6837ea36
shivakumar <42688381+shiva9596@users.noreply.github.com>
2025-04-13 17:25:16 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,43 @@
 from flask import Flask, render_template, jsonify, request
+from dotenv import load_dotenv
 from src.helper import download_hugging_face_embeddings
+from src.prompt import *
 from langchain_pinecone import PineconeVectorStore
 from langchain_openai import OpenAI
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
-from dotenv import load_dotenv
-from src.prompt import *
 import os
-
 
 app = Flask(__name__)
 
+# Load environment variables from .env
 load_dotenv()
 
-PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
-OPENAI_API_KEY=os.environ.get('OPENAI_API_KEY')
+PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 
+# Set them in the environment
 os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
+# Load embedding model
 embeddings = download_hugging_face_embeddings()
 
-
+# Connect to Pinecone index
 index_name = "medicalbot"
-
-# Embed each chunk and upsert the embeddings into your Pinecone index.
 docsearch = PineconeVectorStore.from_existing_index(
     index_name=index_name,
     embedding=embeddings
 )
 
-retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":3})
+# Set up retriever
+retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k": 3})
 
-
+# Load LLM
 llm = OpenAI(temperature=0.4, max_tokens=500)
+
+# Setup RAG prompt
 prompt = ChatPromptTemplate.from_messages(
     [
         ("system", system_prompt),
@@ -42,14 +45,14 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
+# Create chains
 question_answer_chain = create_stuff_documents_chain(llm, prompt)
 rag_chain = create_retrieval_chain(retriever, question_answer_chain)
 
-
+# Routes
 @app.route("/")
 def index():
     return render_template('chat.html')
-
 
 @app.route("/get", methods=["GET", "POST"])
 def chat():
@@ -60,8 +63,6 @@ def chat():
     print("Response : ", response["answer"])
     return str(response["answer"])
 
-
-
-
+# App entry point
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port= 8080, debug= True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
